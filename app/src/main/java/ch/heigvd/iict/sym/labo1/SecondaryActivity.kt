51a4bcdf8eea478b161007188b51df42e5874276
Chiffelle <janischiffelle@hotmail.ch>
2021-10-05 15:43:21 +0200

    4.3 ok

--- app/src/main/java/ch/heigvd/iict/sym/labo1/SecondaryActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo1/SecondaryActivity.kt
@@ -2,10 +2,13 @@ package ch.heigvd.iict.sym.labo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
+import ch.heigvd.iict.sym.labo1.network.ImageDownloader
 
 class SecondaryActivity : AppCompatActivity() {
     private lateinit var email: TextView
+    private lateinit var image: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +19,7 @@ class SecondaryActivity : AppCompatActivity() {
             email.text = bundle.getString("emailInput")
         }
 
+        image = findViewById<ImageView>(R.id.connected_image)
+        ImageDownloader(image, "https://thispersondoesnotexist.com/image").show()
     }
 }
